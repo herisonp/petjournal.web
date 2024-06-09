846e86521a14b9895215dcac9792f591bd6bf2dd
Herison Pereira <herisonup@gmail.com>
2024-06-09 18:41:01 -0300

    style: add z-index on BottomTab

--- src/app/(app)/components/BottomTab.tsx
+++ src/app/(app)/components/BottomTab.tsx
@@ -2,7 +2,7 @@ import { BottomTabItem } from './BottomTabItem';
 
 export function BottomTab() {
   return (
-    <div className="fixed bottom-0 left-0 bg-custom-purple w-full p-4">
+    <div className="fixed z-10 bottom-0 left-0 bg-custom-purple w-full p-4">
       <ul className="flex w-full justify-around">
         <li>
           <BottomTabItem label="Home" to="/" />
