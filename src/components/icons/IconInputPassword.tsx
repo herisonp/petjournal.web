2efbf282520bda2017ed4b507b27cbca7a5ce4ab
Diogo Caxiado <caxiadodev@gmail.com>
2024-06-16 12:16:19 -0300

    feat: Create IconInputPassword component

---
+++ src/components/icons/IconInputPassword.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/utils/twmerge';
+import { IconProps } from './IconProps';
+import Image from 'next/image';
+import toggleShowPassword from '@/assets/svg/show-password.svg';
+import toggleHiddenPassword from '@/assets/svg/hidden-password.svg';
+
+export function IconInputPassword({
+  size = 16,
+  active = false,
+  className,
+}: IconProps) {
+  return (
+    <Image
+      className={cn(className)}
+      src={active ? toggleShowPassword : toggleHiddenPassword}
+      width={size}
+      height={size}
+      alt="Ícone de olho para mostrar e esconder a senha"
+    />
+  );
+}
