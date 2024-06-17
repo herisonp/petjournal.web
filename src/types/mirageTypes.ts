41ac7b517d2db7ab8cea500ca18d0b53c4e812b1
Herison Pereira <herisonup@gmail.com>
2024-06-16 23:16:33 -0300

    chore: clean unused files

--- src/types/mirageTypes.ts
+++
@@ -1,4 +0,0 @@
-export type UserModelType = {
-  email: string;
-  password?: string;
-};
--- src/types/useInputTypes.ts
+++
@@ -1,6 +0,0 @@
-export type ValidationFunction = (value: string) => string | undefined;
-
-export interface UseInput {
-  initialValue?: string;
-  validate?: ValidationFunction;
-}
