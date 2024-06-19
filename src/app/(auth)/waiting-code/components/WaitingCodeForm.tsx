f3679f14f87b52d132e6c14ed3d45dac050567bd
Diogo Caxiado <caxiadodev@gmail.com>
2024-06-19 15:18:58 -0300

    chore: update import paths

--- src/app/(auth)/waiting-code/components/WaitingCodeForm.tsx
+++ src/app/(auth)/waiting-code/components/WaitingCodeForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Button } from '@/components/Button';
-import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/InputOPT';
+import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/Inputs/InputOPT';
 import { submitWaitingCode } from '@/services/submitWaitingCode';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
