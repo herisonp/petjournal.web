4b48e082dfb5f1703da5ac8b50d39f36a1480eb1
Herison Pereira <herisonup@gmail.com>
2024-06-20 15:04:44 -0300

    refactor: remove internal components

--- src/components/Inputs/InputControl.tsx
+++ src/components/Inputs/InputControl.tsx
@@ -1,46 +1,12 @@
-import { InputMessage } from "./InputMessage"
-import { Input } from "./Input"
-import { Label } from "../Label"
-import { cn } from "@/utils/twmerge";
+import { cn } from '@/utils/twmerge';
 
 interface InputControlProps {
-   variant?: 'primary' | 'secondary';
-   className?: string;
-   label: string;
-   htmlFor?: React.LabelHTMLAttributes<HTMLLabelElement>['htmlFor'];
-   type: React.InputHTMLAttributes<HTMLInputElement>['type'];
-   placeholder?: React.InputHTMLAttributes<HTMLInputElement>['placeholder'];
-   message?: string;
-   styleMessage?: 'error' | 'warning';
-} 
-
-const InputControl = ({variant = 'primary', className, label, htmlFor, type, placeholder, message, styleMessage}: InputControlProps) => {
-  const labelClassName = variant === 'secondary' ? 'pl-2' : '';
-  let inputControl;
-  
-  switch (styleMessage) {
-    case 'error' : 
-      inputControl = (
-        <div className={cn(className)}>
-          <Label variant={variant} htmlFor={htmlFor} className={labelClassName}>{label}</Label>
-          <Input variant='error' type={type} placeholder={placeholder} />
-          <InputMessage message={message} variant={styleMessage} />
-        </div>
-      );
-    break;
-
-    default: 
-      inputControl = (
-        <div className={cn(className)}>
-          <Label variant={variant} htmlFor={htmlFor} className={labelClassName}>{label}</Label>
-          <Input variant={variant} type={type} placeholder={placeholder} />
-          <InputMessage message={message} variant={styleMessage} />
-        </div>
-      )
-    break;
-  }
-
-  return inputControl;
+  children: React.ReactNode;
+  className: string;
 }
 
-export { InputControl }
+const InputControl = ({ children, className }: InputControlProps) => {
+  return <div className={cn('flex flex-col gap-1', className)}>{children}</div>;
+};
+
+export { InputControl };
