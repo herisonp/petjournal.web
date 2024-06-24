bf1ec002455d3b744b230309c00777fc69143654
Herison Pereira <herisonup@gmail.com>
2024-06-24 20:29:58 -0300

    style: change state styles Select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,16 +1,18 @@
-import * as React from "react"
-import * as SelectPrimitive from "@radix-ui/react-select"
-import { ChevronDown } from "lucide-react"
-import { IconErrorCircle } from './icons/IconErrorCircle'
-import { cn } from "@/utils/twmerge"
+import * as React from 'react';
+import * as SelectPrimitive from '@radix-ui/react-select';
+import { CheckIcon, ChevronDown } from 'lucide-react';
+import { IconErrorCircle } from './icons/IconErrorCircle';
+import { cn } from '@/utils/twmerge';
 
-const SelectStyles = 'flex h-10 w-full items-center justify-between rounded-xl bg-white px-2 py-2 text-sm border'
+const SelectStyles =
+  'flex h-10 w-full items-center justify-between rounded-xl bg-white px-2 py-2 text-sm border';
 
-const Select = SelectPrimitive.Root
-const SelectGroup = SelectPrimitive.Group
-const SelectValue = SelectPrimitive.Value
+const Select = SelectPrimitive.Root;
+const SelectGroup = SelectPrimitive.Group;
+const SelectValue = SelectPrimitive.Value;
 
-interface SelectTriggerProps extends React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger> {
+interface SelectTriggerProps
+  extends React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger> {
   error?: boolean;
 }
 
@@ -21,49 +23,54 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      error ? `${SelectStyles} border-red-300 border-solid outline-none` 
-      : `${SelectStyles} border-[#B2B2B2] border-dashed outline-none focus:border-[#B78AF7] focus:border-solid`,
-      className
+      error
+        ? `${SelectStyles} border-red-300 border-solid outline-none`
+        : `${SelectStyles} border-[#B2B2B2] border-dashed outline-none focus:border-[#B78AF7] focus:border-solid data-[state=open]:border-[#B78AF7] data-[state=open]:border-solid`,
+      className,
     )}
     {...props}
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      {error ? <IconErrorCircle className="h-5 w-5 text-red-500" /> : <ChevronDown className="h-4 w-4 text-[#2E2E2E]" />}
+      {error ? (
+        <IconErrorCircle className="h-5 w-5 text-red-500" />
+      ) : (
+        <ChevronDown className="h-4 w-4 text-[#2E2E2E]" />
+      )}
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
-))
-SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
+));
+SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
 
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
+>(({ className, children, position = 'popper', ...props }, ref) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 max-h-56 min-w-[8rem] rounded-xl bg-white border border-[#B78AF7]",
-        position === "popper" &&
-          "data-[side=bottom]:translate-y-2 data-[side=left]:-translate-x-2 data-[side=right]:translate-x-2 data-[side=top]:-translate-y-2",
-        className
+        'relative z-50 max-h-56 min-w-[8rem] rounded-xl bg-white border border-[#B78AF7]',
+        position === 'popper' &&
+          'data-[side=bottom]:translate-y-2 data-[side=left]:-translate-x-2 data-[side=right]:translate-x-2 data-[side=top]:-translate-y-2',
+        className,
       )}
       position={position}
       {...props}
     >
       <SelectPrimitive.Viewport
         className={cn(
-          "p-1",
-          position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+          'p-1',
+          position === 'popper' &&
+            'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]',
         )}
       >
         {children}
       </SelectPrimitive.Viewport>
     </SelectPrimitive.Content>
   </SelectPrimitive.Portal>
-))
-SelectContent.displayName = SelectPrimitive.Content.displayName
+));
+SelectContent.displayName = SelectPrimitive.Content.displayName;
 
 const SelectItem = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Item>,
@@ -72,15 +79,20 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "flex w-full cursor-pointer select-none items-center rounded-xl py-2 pl-4 text-sm outline-none",
-      className
+      'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[state=checked]:text-studio-600',
+      className,
     )}
     {...props}
   >
+    <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
+      <SelectPrimitive.ItemIndicator>
+        <CheckIcon className="h-4 w-4" />
+      </SelectPrimitive.ItemIndicator>
+    </span>
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
   </SelectPrimitive.Item>
-))
-SelectItem.displayName = SelectPrimitive.Item.displayName
+));
+SelectItem.displayName = SelectPrimitive.Item.displayName;
 
 const SelectSeparator = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Separator>,
@@ -88,11 +100,11 @@ const SelectSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 my-1 h-px ", className)}
+    className={cn('-mx-1 my-1 h-px ', className)}
     {...props}
   />
-))
-SelectSeparator.displayName = SelectPrimitive.Separator.displayName
+));
+SelectSeparator.displayName = SelectPrimitive.Separator.displayName;
 
 export {
   Select,
@@ -102,4 +114,4 @@ export {
   SelectContent,
   SelectItem,
   SelectSeparator,
-}
+};
