36308401bcde45291171ce5279808162d741935f
Diogo Caxiado <caxiadodev@gmail.com>
2024-07-01 22:10:47 -0300

    refactor: switching to using the AriaKit library

--- src/components/ComboBox/ComboBox.tsx
+++ src/components/ComboBox/ComboBox.tsx
@@ -1,67 +1,37 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ChevronDown } from "lucide-react"
-import { Command, CommandEmpty, CommandGroup, CommandItem, CommandList } from "@/components/ComboBox/Command"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ComboBox/Popover"
-import { ScrollArea } from "../ScrollArea"
-import { Input } from "../Inputs/Input"
+import { Breed } from "@/types/PetsTypes";
+import * as Ariakit from "@ariakit/react";
+import { ScrollArea } from "../ScrollArea";
+import { ChevronDown } from "lucide-react";
 
-interface ComboBoxProps {
+interface SearchInputProps {
   placeholder: string;
-  items: { id: string; name: string; }[];
+  items: Breed[];
 }
 
-export function ComboBox({ placeholder, items }: ComboBoxProps) {
-  const [open, setOpen] = React.useState(false)
-  const [inputValue, setInputValue] = React.useState("")
-
-  const handleSelect = (currentValue: string) => {
-    setInputValue(items.find(item => item.name === currentValue)?.name || "")
-    setOpen(false)
-  }
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value)
-  }
-
+function SearchInput({placeholder, items}: SearchInputProps) {
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild className="w-full max-w-[327px]">
-        <div className="flex relative justify-between items-center">
-          <Input
-            type="text"
-            variant="secondary"
-            aria-expanded={open}
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder={placeholder}
-            className={`rounded-2xl text-sm ${open ? "border-solid border border-[#B78AF7] shadow-custom-select" : ""}`}
-          />
-          <ChevronDown className="absolute right-2" size={18} color="#2E2E2E"/>
-        </div>
-      </PopoverTrigger>
-      <PopoverContent>
-        <Command>
-          <CommandList>
-            <CommandEmpty>Não encontrado.</CommandEmpty>
-            <ScrollArea className="h-[150px]">
-              <CommandGroup>
-                {items.filter(item => item.name.toLowerCase().includes(inputValue.toLowerCase())).map((item) => (
-                  <CommandItem
-                    key={item.value}
-                    value={item.value}
-                    onSelect={handleSelect}
-                    className="pl-1"
-                  >
-                    {item.name}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </ScrollArea>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
-  )
+    <Ariakit.ComboboxProvider>
+      <div className="w-full max-w-[327px] flex relative justify-center items-center">
+        <Ariakit.Combobox placeholder={placeholder} className="w-full h-10 p-2 border border-[#B2B2B2] border-dashed rounded-2xl text-[#2E2E2E] focus:border-[#B78AF7] focus:shadow-custom-select" />
+        <ChevronDown className="absolute right-2" size={18} color="#2E2E2E" />
+      </div>
+      <Ariakit.ComboboxPopover gutter={8} sameWidth className="border rounded-2xl p-2">
+        <ScrollArea className="h-[150px]">
+          {items.map((item) => (
+            <Ariakit.ComboboxItem
+              className="p-2 font-normal text-sm" 
+              value={item.name} 
+              key={item.id}
+            >
+              {item.name}
+            </Ariakit.ComboboxItem>
+          ))}
+         </ScrollArea>
+      </Ariakit.ComboboxPopover>
+    </Ariakit.ComboboxProvider>
+  );
 }
+
+export { SearchInput };
