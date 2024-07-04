0888b57458c312abf44bd20c244c222833bb5b37
Herison Pereira <herisonup@gmail.com>
2024-07-04 20:08:26 -0300

    refactor: remove unuseds components

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import { BannerHome } from './components/BannerHome';
 import { Header } from '../components/Header';
 import { Services } from './components/Services';
-import { SearchInput } from '@/components/Fields/SearchInput';
 
 export default function DashboardPage() {
   return (
@@ -14,7 +13,6 @@ export default function DashboardPage() {
           <h2 className="text-lg">Serviços</h2>
           <Link href="#">ver mais</Link>
         </div>
-        <SearchInput placeholder='Teste' items={[{label: 'Cachorro', value: 'Cachorro'},{label: 'Gato', value: 'Gato'},{label: 'Cachorro Grande', value: 'Cachorro Grande'},{label: 'Cachorro Pequeno', value: 'Cachorro Pequeno'},{label: 'Cachorro Médio', value: 'Cachorro Médio'}]}/>
         <Services />
       </main>
     </>
