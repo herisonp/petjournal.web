262c364928bab2986ea9a4761819b05f3a0d62a1
Diogo Caxiado <caxiadodev@gmail.com>
2024-07-12 18:41:26 -0300

    style: resolve AddPet component bug

--- src/app/(app)/pets/components/AddPet.tsx
+++ src/app/(app)/pets/components/AddPet.tsx
@@ -8,8 +8,8 @@ type AddPetTypes = {
 
 export function AddPet({ href }: AddPetTypes) {
   return (
-    <div className="fixed z-10 bottom-28 right-4 w-14 h-14 flex justify-center items-center rounded-full bg-[#7C54A7]">
-      <Link href={href}>
+    <div className="fixed z-10 bottom-28 right-4 w-14 h-14 rounded-full bg-[#7C54A7]">
+      <Link className="w-full h-full flex justify-center items-center" href={href}>
         <Image src={Button} alt="botão de adicionar um novo pet" />
       </Link>
     </div>
