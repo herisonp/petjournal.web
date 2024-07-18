79abd876012ffabba2ffe664145bb4cdbf6a5e0d
Luzin7 <lvictor.dutra@gmail.com>
2024-07-18 16:22:19 -0300

    refactor: add zod and hook form to validate register
    replace input to component Input

--- src/app/(auth)/register/components/RegisterForm.tsx
+++ src/app/(auth)/register/components/RegisterForm.tsx
@@ -4,24 +4,44 @@ import { useRouter } from 'next/navigation';
 import { submitLogin } from '@/services/submitLogin';
 import { submitRegister } from '@/services/submitRegister';
 import { Button } from '@/components/Button';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { UserRegisterProps, userRegisterSchema } from '@/schemas/player';
+import { Input } from '@/components/Fields/Input';
 
 export function RegisterForm() {
   const [isLoading, setIsLoading] = useState(false);
   const [remember, setRemember] = useState(false);
   const router = useRouter();
 
-  async function handleSubmitRegister(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<UserRegisterProps>({
+    resolver: zodResolver(userRegisterSchema),
+    criteriaMode: 'firstError',
+    reValidateMode: 'onChange',
+    mode: 'onBlur',
+  });
+
+  async function handleSubmitRegister({
+    email,
+    firstName,
+    lastName,
+    password,
+    passwordConfirmation,
+    phone,
+  }: UserRegisterProps) {
     setIsLoading(true);
-    const formData = new FormData(event.currentTarget);
     try {
       const newUser = {
-        firstName: formData.get('name') as string,
-        lastName: formData.get('lastname') as string,
-        email: formData.get('email') as string,
-        password: formData.get('password') as string,
-        passwordConfirmation: formData.get('password-confirm') as string,
-        phone: formData.get('phone') as string,
+        firstName,
+        lastName,
+        email,
+        password,
+        passwordConfirmation,
+        phone,
         isPrivacyPolicyAccepted: remember,
       };
 
@@ -44,93 +64,116 @@ export function RegisterForm() {
   }
 
   return (
-    <form onSubmit={handleSubmitRegister} className="flex flex-col gap-y-4">
+    <form
+      onSubmit={handleSubmit(handleSubmitRegister)}
+      className="flex flex-col gap-y-4"
+    >
       <label>
         <div className="text-custom-purple text-sm font-medium">Nome</div>
-        <div className="border border-[#1b1b1b] rounded-[5px] py-2 px-1">
-          <input
-            type="text"
-            className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
-            name="name"
-            id="name"
-            placeholder="Digite seu primeiro nome"
-          />
-        </div>
+        <Input
+          type="text"
+          id="name"
+          placeholder="Digite seu primeiro nome"
+          {...register('firstName')}
+        />
+        {errors.firstName && (
+          <span className="text-red-500 text-xs">
+            {errors.firstName.message}
+          </span>
+        )}
       </label>
       <label>
         <div className="text-custom-purple text-sm font-medium">Sobrenome</div>
-        <div className="border border-[#1b1b1b] rounded-[5px] py-2 px-1">
-          <input
-            type="text"
-            className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
-            name="lastname"
-            id="lastname"
-            placeholder="Digite seu sobrenome"
-          />
-        </div>
+        <Input
+          type="text"
+          id="lastname"
+          placeholder="Digite seu sobrenome"
+          {...register('lastName')}
+        />
+        {errors.lastName && (
+          <span className="text-red-500 text-xs">
+            {errors.lastName.message}
+          </span>
+        )}
       </label>
       <label>
         <div className="text-custom-purple text-sm font-medium">Email</div>
-        <div className="border border-[#1b1b1b] rounded-[5px] py-2 px-1">
-          <input
-            type="email"
-            className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
-            name="email"
-            id="email"
-            placeholder="E-mail"
-          />
-        </div>
+        <Input
+          type="email"
+          id="email"
+          placeholder="E-mail"
+          {...register('email')}
+        />
+        {errors.email && (
+          <span className="text-red-500 text-xs">{errors.email.message}</span>
+        )}
       </label>
       <label>
         <div className="text-custom-purple text-sm font-medium">Telefone</div>
-        <div className="border border-[#1b1b1b] rounded-[5px] py-2 px-1">
-          <input
-            type="text"
-            className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
-            name="phone"
-            id="phone"
-            placeholder="Telefone"
-          />
-        </div>
+        <Input
+          type="text"
+          className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
+          id="phone"
+          placeholder="Telefone"
+          {...register('phone')}
+        />
+        {errors.phone && (
+          <span className="text-red-500 text-xs">{errors.phone.message}</span>
+        )}
       </label>
       <label>
         <div className="text-custom-purple text-sm font-medium">Senha</div>
-        <div className="border border-[#1b1b1b] rounded-[5px] py-2 px-1">
-          <input
-            type="password"
-            className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
-            name="password"
-            id="password"
-            placeholder="Senha"
-          />
-        </div>
+        <Input
+          type="password"
+          className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
+          id="password"
+          placeholder="Senha"
+          {...register('password')}
+        />
+        {errors.password && (
+          <span className="text-red-500 text-xs">
+            {errors.password.message}
+          </span>
+        )}
       </label>
       <label>
         <div className="text-custom-purple text-sm font-medium">
           Confirmar senha
         </div>
-        <div className="border border-[#1b1b1b] rounded-[5px] py-2 px-1">
-          <input
-            type="password"
-            className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
-            name="password-confirm"
-            id="password-confirm"
-            placeholder="Confirmar senha"
-          />
-        </div>
+        <Input
+          type="password"
+          className="w-full outline-0 text-[#292929] font-medium placeholder:text-[#BFBFBF]"
+          id="password-confirm"
+          placeholder="Confirmar senha"
+          {...register('passwordConfirmation')}
+        />
+        {errors.passwordConfirmation && (
+          <span className="text-red-500 text-xs">
+            {errors.passwordConfirmation.message}
+          </span>
+        )}
       </label>
       <label className="flex items-center relative">
         <input
           className="appearance-none w-5 h-5 rounded-full border-2 border-custom-purple mr-1"
           type="checkbox"
           checked={remember}
-          onChange={(event) => setRemember(event.target.checked)}
+          {...register('isPrivacyPolicyAccepted', {
+            onChange(event) {
+              setRemember(event.target.checked);
+            },
+          })}
         />
         {remember && (
           <div className="absolute w-2 h-2 bg-custom-purple rounded-full left-[0.375rem]"></div>
         )}
         <span>Eu concordo com a politica de privacidade</span>
       </label>
+      {errors.isPrivacyPolicyAccepted && (
+        <span className="text-red-500 text-xs">
+          {errors.isPrivacyPolicyAccepted.message}
+        </span>
+      )}
       <Button className="mt-16" type="submit" disabled={!!isLoading}>
         {isLoading ? 'Enviando...' : 'Continuar'}
       </Button>
