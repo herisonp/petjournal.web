480954f1eb16560e9a4825cc89d9aed1795dcb89
Diogo Caxiado <caxiadodev@gmail.com>
2024-07-19 14:25:00 -0300

    refactor: ChangePasswordSchema messages changes

--- src/schemas/ChangePassword.ts
+++ src/schemas/ChangePassword.ts
@@ -2,18 +2,18 @@ import { z } from 'zod';
 
 export const ChangePasswordSchema = z.object({
   password: z.string()
-    .min(8, {message: 'Senha inválida.'})
-    .regex(/[a-z]/, 'Senha inválida')
-    .regex(/[A-Z]/, 'Senha inválida')
-    .regex(/[0-9]/, 'Senha inválida')
-    .regex(/[@$!%*?&#]/, 'Senha inválida')
+    .min(8, 'A senha deve ter, no mínimo, 8 caracteres')
+    .regex(/[a-z]/, 'Deve conter pelo menos uma letra minúscula')
+    .regex(/[A-Z]/, 'Deve conter pelo menos uma letra maiúscula')
+    .regex(/[0-9]/, 'Deve conter pelo menos um número')
+    .regex( /[@$!%*?&#]/, 'A senha deve conter pelo menos um símbolo especial (@, $, !, %, *, ?, &, #).')
     .trim(),
   passwordConfirmation: z.string()
-    .min(6, {message: 'Senha inválida.'})
-    .regex(/[a-z]/, 'Senha inválida')
-    .regex(/[A-Z]/, 'Senha inválida')
-    .regex(/[0-9]/, 'Senha inválida')
-    .regex(/[@$!%*?&#]/, 'Senha inválida')
+    .min(8, 'A senha deve ter, no mínimo, 8 caracteres')
+    .regex(/[a-z]/, 'Deve conter pelo menos uma letra minúscula')
+    .regex(/[A-Z]/, 'Deve conter pelo menos uma letra maiúscula')
+    .regex(/[0-9]/, 'Deve conter pelo menos um número')
+    .regex(/[@$!%*?&#]/, 'A senha deve conter pelo menos um símbolo especial (@, $, !, %, *, ?, &, #).')
     .trim(),
   confirmationAction: z.boolean().refine(confirmation => confirmation === true, {
     message: 'Você deve marcar esta opção para continuar.',
